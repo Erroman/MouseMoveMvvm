5803070c5e2db44c8d6ccf70d928bee9db939587
Vladimir Romanovsky <Vladimir_Roman@mail.ru>
2019-05-10 01:27:14 +0500

    Microsoft.Xaml.Behaviors.Wpf is added from Nuget repository

--- MouseMoveMvvm/MainWindowViewModel .cs
+++ MouseMoveMvvm/MainWindowViewModel .cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 namespace MouseMoveMvvm
 {
-    class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        private double _panelX;
+        private double _panelY;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double PanelX
+        {
+            get { return _panelX; }
+            set
+            {
+                if (value.Equals(_panelX)) return;
+                _panelX = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double PanelY
+        {
+            get { return _panelY; }
+            set
+            {
+                if (value.Equals(_panelY)) return;
+                _panelY = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
